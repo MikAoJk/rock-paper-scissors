d4301f7dbd12f40fb466db46ed4fceb53c1739a5
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-12-20 13:52:14 +0100

    Added in diffrent text for diffent winners

--- src/components/GameResult.tsx
+++ src/components/GameResult.tsx
@@ -4,8 +4,11 @@ import {GamePlay} from "@/components/Game";
 const GameResult = (gamePlay: GamePlay) => {
 
     return (
-        <div className="m-2 md:m-5 md:text-l">
-            <p className="text-2xl">Winner: {gamePlay.winner}</p>
+        <div className="m-2 md:m-5 md:text-l text-left">
+            {gamePlay.winner == 'You' && <p className="text-2xl">You won!</p>}
+            {gamePlay.winner == 'Computer' && <p className="text-2xl">Computer won</p>}
+            {gamePlay.winner == 'Draw' && <p className="text-2xl">Its a draw</p>}
+
             <div className="mt-4">
                 <p>Your choice: {gamePlay.playerVal}</p>
                 <p>Computers choice: {gamePlay.computerVal}</p>
